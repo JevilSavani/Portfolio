50a4ddcb1668723e6106be86996adebbf8578043
JevilSavani <jevilsavani@gmail.com>
2026-07-20 23:22:28 +0530

    Add Section And Header And Footer

--- student-portfolio/src/Pages/about.jsx
+++ student-portfolio/src/Pages/about.jsx
@@ -1,16 +1,16 @@
 function About() {
-  return (
-    <section className="section about-section">
-      <div className="section-title">
-        <span></span>
-        <h2>About Me</h2>
-      </div>
-      <p>
-        I am a passionate software engineering student focusing on Machine Learning,Deep Learning
-        ,Web Development and component-driven UI architecture.
-      </p>
-    </section>
-  )
+    return (
+        <section className="section about-section">
+            <div className="section-title">
+                <span></span>
+                <h2>About Me</h2>
+            </div>
+            <p>
+                I am a passionate software engineering student focusing on Machine Learning,Deep Learning
+                ,Web Development and component-driven UI architecture.
+            </p>
+        </section>
+    )
 }
 
 export default About
